b7eeac4cb0a754e11e85af051180782546f83f57
tomerwolfson <tomer.wolfson@gmail.com>
2015-12-11 18:20:09 +0200

    changed notes

--- slp/src/slp/ClassMethod.java
+++ slp/src/slp/ClassMethod.java
@@ -4,42 +4,36 @@ import java.util.List;
 
 public class ClassMethod extends Method {
 
-		/** Accepts a visitor object as part of the visitor pattern.
-		 * @param visitor A visitor.
-		 */
-		@Override
-		public void accept(Visitor visitor) {
-			visitor.visit(this);
-		}
-	
-		/** Accepts a propagating visitor parameterized by two types.
-		 * 
-		 * @param <DownType> The type of the object holding the context.
-		 * @param <UpType> The type of the result object.
-		 * @param visitor A propagating visitor.
-		 * @param context An object holding context information.
-		 * @return The result of visiting this node.
-		 */
-		@Override
-		public <DownType, UpType> UpType accept(
-				PropagatingVisitor<DownType, UpType> visitor, DownType context) {
-			return visitor.visit(this, context);
-		}
+	/** Accepts a visitor object as part of the visitor pattern.
+	 * @param visitor A visitor.
+	 */
+	@Override
+	public void accept(Visitor visitor) {
+		visitor.visit(this);
+	}
+
+	/** Accepts a propagating visitor parameterized by two types.
+	 * 
+	 * @param <DownType> The type of the object holding the context.
+	 * @param <UpType> The type of the result object.
+	 * @param visitor A propagating visitor.
+	 * @param context An object holding context information.
+	 * @return The result of visiting this node.
+	 */
+	@Override
+	public <DownType, UpType> UpType accept(
+			PropagatingVisitor<DownType, UpType> visitor, DownType context) {
+		return visitor.visit(this, context);
+	}
 
 	/**
-	 * Constructs a new static method node.
-	 * 
-	 * @param type
-	 *            Data type returned by method.
-	 * @param name
-	 *            Name of method.
-	 * @param formals
-	 *            List of method parameters.
-	 * @param statements
-	 *            List of method's statements.
+	 * Constructs new Class Method AST node
+	 * @param type - method's return type
+	 * @param name - method name
+	 * @param formals - method parameters
+	 * @param statements - method's statements
 	 */
-	public ClassMethod(Type type, String name, List<Formal> formals,
-			StmtList statements) {
+	public ClassMethod(Type type, String name, List<Formal> formals, StmtList statements){
 		super(type, name, formals, statements);
 		this.is_static = false;
 	}
