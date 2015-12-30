2c3e3cc6b73728e81add3ce8a8d1948cb698e910
tomerwolfson <tomer.wolfson@gmail.com>
2015-12-31 01:46:49 +0200

    problem with test: objects.ic, assignment of Reg8.1 to Reg8 when Reg8 contains the class 'this'

--- slp/src/lir/LirVisitor.java
+++ slp/src/lir/LirVisitor.java
@@ -232,7 +232,6 @@ public class LirVisitor implements PropagatingVisitor<Environment,LirReturnInfo>
 			if(valueInfo.getMoveCommand() != MoveEnum.MOVE){
 				// first, store value in register 
 				valueReg = "R"+d.getCurrentRegister();
-				d.incrementRegister();
 				d.addInstructionToBuilder(valueInfo.getMoveCommand(), value, valueReg);
 				// then assign the value in the register to location
 				d.addInstructionToBuilder(locationInfo.getMoveCommand(), valueReg, locationReg);
